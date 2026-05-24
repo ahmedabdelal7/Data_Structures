f18b4325b156a04f4157b416b01da528ede46c8b
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-05-24 22:29:40 +0300

    Add Stack Using Queue

---
+++ Data Structure Level 1/3-Stack Using Queue/README.md
@@ -0,0 +1,2 @@
+#c13-p3-MyStack
+Project 2 My Stack Implementation Based On Queue Class In CPP At Course 13
---
+++ Data Structure Level 1/3-Stack Using Queue/clsMyStack.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "clsMyQueue.h"
+
+template<typename T>
+class clsMyStack : public clsMyQueue<T>
+{
+public:
+	
+	void push(T Value) {
+		clsMyQueue<T>::_MyList.InsertAtBeginning(Value);
+	}
+
+	T Top() {
+		return clsMyQueue<T>::front();
+	}
+	
+	T Bottom() {
+		return clsMyQueue<T>::back();
+	}
+
+};
+
