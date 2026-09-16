62753deb6f36394378590244c1b78b15c3f5ded9
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-09-16 17:31:26 +0300

    remove from list

---
+++ Data Structures Level 2 in C#/02-Lists/03-Remove From List/Ex-1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+
+        List<string> students = new List<string>
+        {
+            "Ahmed",
+            "Mohamed",
+            "Omar",
+            "Ali",
+            "Hassan"
+        };
+
+
+        Console.WriteLine($"List Before Editing Count = {students.Count}: {string.Join(", ", students)}");
+
+        //Remove Omar from Course:
+        students.Remove("Omar");
+        Console.WriteLine($"List After Remove 'Omar' Count = {students.Count}: {string.Join(", ", students)}");
+
+
+        //Remove The First Student:
+        students.RemoveAt(0);
+        Console.WriteLine($"List After Remove First Student Count = {students.Count}: {string.Join(", ", students)}");
+
+        //Remove All Students There Name Starts With 'A':
+        students.RemoveAll((s) => s.StartsWith("A"));
+        Console.WriteLine($"List After Remove Students Start With 'A' Count = {students.Count}: {string.Join(", ", students)}");
+
+        //At The End Of The Day, Remove All Students:
+        students.Clear();
+        Console.WriteLine($"List After Clear Count = {students.Count}: {string.Join(", ", students)}");
+
+
+        // Waiting for a key press
+        Console.ReadKey();
+    }
+}
---
+++ Data Structures Level 2 in C#/02-Lists/03-Remove From List/Ex-2 Cart Items.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        //Shopping Cart:
+        List<string> cart = new List<string>
+        {
+            "Laptop",
+            "Mouse",
+            "Keyboard",
+            "Monitor",
+            "Headset",
+            "USB Cable"
+        };
+
+        // Clear() complexity -> value type -> O(1) 
+        // Clear() complexity -> Reference type > O(n)
+
+        Console.WriteLine($"Cart Before Editing Count = {cart.Count}: {string.Join(", ", cart)}");
+
+        //Customer Cancelled 'Mouse':
+        cart.Remove("Mouse");
+        Console.WriteLine($"Cart After Cancel 'Mouse' Count = {cart.Count}: {string.Join(", ", cart)}");
+
+        //Customer Removed First Item In cart:
+        cart.RemoveAt(0);
+        Console.WriteLine($"Cart After Remove First Item Count = {cart.Count}: {string.Join(", ", cart)}");
+
+        //Remove all Items Contains "USB" or "Headset":
+        cart.RemoveAll((item) => item.Contains("USB") || item.Contains("Headset"));
+        Console.WriteLine($"Cart After Remove Items Contains \"USB\" or \"Headset\" Count = {cart.Count}: {string.Join(", ", cart)}");
+
+        //Done Checkout now clear the cart:
+        cart.Clear();
+        Console.WriteLine($"Cart After Clear Count = {cart.Count}: {string.Join(", ", cart)}");
+
+
+        // Waiting for a key press
+        Console.ReadKey();
+    }
+}
