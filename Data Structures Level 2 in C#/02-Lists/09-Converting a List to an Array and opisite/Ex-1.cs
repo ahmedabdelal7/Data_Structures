0cdc3b5c0435aa857dd2ea815673f74ef623b71c
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-09-17 19:33:45 +0300

    Converting a List to an Array

---
+++ Data Structures Level 2 in C#/02-Lists/09-Converting a List to an Array and opisite/Ex-1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // List initialization
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+        // Converting the list to an array
+        int[] numbersArray = numbers.ToArray();
+        Console.WriteLine("Array elements: " + string.Join(", ", numbersArray));
+
+
+        // Converting the Array to an List
+        List<int> numbersList =  numbersArray.ToList();
+        Console.WriteLine("List elements: " + string.Join(", ", numbersList));
+
+
+
+        Console.ReadKey();
+    }
+}
+
