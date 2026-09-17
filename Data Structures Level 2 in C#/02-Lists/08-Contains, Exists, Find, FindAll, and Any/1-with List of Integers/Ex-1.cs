f1fae1de99559f58393957ed27e18e109fa141b1
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-09-17 15:14:49 +0300

    Contains, Exists, Find, FindAll, and Any

---
+++ Data Structures Level 2 in C#/02-Lists/08-Contains, Exists, Find, FindAll, and Any/1-with List of Integers/Ex-1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Creates and initializes a list of integers
+        List<int> numbers = new List<int> { 44, 22, 55, 666, 9, -6, 345, 11, 3, 3 };
+
+
+        // Contains: Checks if the list contains a specific value
+        Console.WriteLine("List contains 9: " + numbers.Contains(9));
+
+
+        // Exists: Checks if at least one element matches a condition
+        Console.WriteLine("List contains negative numbers: " + numbers.Exists(n => n < 0));
+
+
+        // Find: Returns the first element that matches a condition
+        Console.WriteLine("First negative number: " + numbers.Find(n => n < 0));
+
+
+        // FindAll: Returns all elements that match a condition
+        Console.WriteLine("All negative numbers: " + string.Join(", ", numbers.FindAll(n => n < 0)));
+
+
+        // Any: Checks if at least one element matches a condition
+        Console.WriteLine("Any numbers greater than 100: " + numbers.Any(n => n > 100));
+
+
+        // Waits for a key press before closing the console
+        Console.ReadKey();
+    }
+}
---
+++ Data Structures Level 2 in C#/02-Lists/08-Contains, Exists, Find, FindAll, and Any/1-with List of Integers/Ex-2 Tranasctons.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        //Positive numbers = Deposits
+        //Negative numbers = Withdrawals
+
+        List<int> transactions = new List<int>
+        {
+            250, -100, 500, -50, 1200, 300, -250, 75, 900, -700
+        };
+
+        // Check if the transaction 1200 exists
+        Console.WriteLine($"Is Transaction 1200 Exists: {transactions.Contains(1200)}");
+
+        // Find the first withdrawal greater than 200 in absolute value
+        Console.WriteLine($"First Withdrawal > 200 in Absolute: " +
+            $"{transactions.Find(t => t < 0 && Math.Abs(t) > 200)}");
+
+        // Find all transactions with an absolute value of at least 500 and sort them descending
+        Console.WriteLine($"Transactions with an absolute value of at least 500 Desc: ");
+            transactions.FindAll(t => Math.Abs(t) >= 500)
+            .OrderByDescending(t => t)
+            .ToList()
+            .ForEach(t => Console.WriteLine(t));
+
+        // Check if there is any suspicious withdrawal greater than 600
+        Console.WriteLine($"Is There is any withdrawal > 600: " +
+            $"{transactions.Any(t => t < 0 && Math.Abs(t) > 600)}"); 
+
+        Console.ReadKey();
+    }
+}
