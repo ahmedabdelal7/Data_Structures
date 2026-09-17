26cf58fd3db6ef2775ea1a69fc85929a7722deaa
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-09-17 16:55:46 +0300

    Contains, Exists, Find, FindAll, and Any

---
+++ Data Structures Level 2 in C#/02-Lists/08-Contains, Exists, Find, FindAll, and Any/2-with List of Strings/Ex-2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        //Registered Users:
+        List<string> usernames = new List<string>
+        {
+            "ahmed_dev",
+            "mohamed123",
+            "admin",
+            "sara_cs",
+            "omar2004",
+            "test_user",
+            "hany_dev",
+            "root"
+        };
+
+        // Check if the username "admin" is already registered
+        Console.WriteLine($"Is 'admin' Registered: {usernames.Contains("admin")}");
+
+        // Find the first username containing "dev"
+        Console.WriteLine($"FIrst UserName Contains 'dev': {usernames.Find(u=> u.Contains("dev"))}");
+        
+        // Find all usernames containing at least one digit
+        Console.WriteLine("User Names Contains at least one digit: ");
+        usernames.FindAll(u => u.Any(c => char.IsDigit(c)))
+            .ForEach(u => Console.WriteLine(u));
+
+
+        // Check if any username violates the registration rules
+        //Long at least 8 char and don't start with 'admin' or 'root'
+        Console.WriteLine("Is any user name violates rules: " +
+            usernames.Exists(u => u.Length < 8
+                || u.StartsWith("admin")
+                || u.StartsWith("root"))
+        );
+
+
+        Console.ReadKey();
+    }
+}
---
+++ Data Structures Level 2 in C#/02-Lists/08-Contains, Exists, Find, FindAll, and Any/2-with List of Strings/Ex-3 Files.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> files = new List<string>
+        {
+            "report.pdf",
+            "report2.pdf",
+            "photo.jpg",
+            "program.cs",
+            "database.sql",
+            "notes.txt",
+            "logo.png",
+            "main.cs",
+            "backup.sql",
+            "presentation.pptx",
+            "resume.pdf",
+            "data.csv",
+            "config.json"
+        };
+
+        // Check if "database.sql" exists in the file list
+        Console.WriteLine($"Is 'database.sql' Exists: " +
+            $"{files.Contains("database.sql")}");
+
+        // Find the first PDF file
+        Console.WriteLine($"The first PDF file: " +
+            $"{files.Find(f => f.EndsWith(".pdf"))}");
+
+        // Display all C# source files
+        Console.WriteLine("All files '.cs' Is: ");
+        files.FindAll(f => f.EndsWith(".cs"))
+            .ForEach(f => Console.WriteLine(f));
+
+        // Check if any file starts with "backup"
+        Console.WriteLine($"Is Any file starts with 'backup': " +
+            $"{files.Exists(f => f.StartsWith("backup"))}");
+
+        // Check if any file name is longer than 15 characters
+        Console.WriteLine($"Is any file name longer than 15 char: " +
+            $"{files.Any(f => f.Length > 15)}");
+
+        // Display files containing "data" or "config"
+        Console.WriteLine($"All files containing 'data' or 'config': ");
+        files.FindAll(f => f.Contains("data") || f.Contains("config"))
+            .ForEach(f => Console.WriteLine(f));
+
+        // Find the first backup SQL file 
+        Console.WriteLine($"First backup sql file: " +
+            $"{files.Find(f => f.Contains("backup") && f.EndsWith(".sql"))}");
+
+        // Display all files that are not PDF files
+        Console.WriteLine($"All files not end with PDF: ");
+        files.FindAll(f => !f.EndsWith(".pdf"))
+            .ForEach(f => Console.WriteLine(f));
+
+        // Check if there are any files with unsupported extensions
+        // Allowed extinctions -> .pdf - .jpg - .png - .txt - .cs - .sql
+        Console.WriteLine($"Is any file with unsupported extinction: " +
+            $@"{files.Any(
+                f => !(f.EndsWith(".pdf")
+                || f.EndsWith(".jpg")
+                || f.EndsWith(".png")
+                || f.EndsWith(".txt")
+                || f.EndsWith(".cs")
+                || f.EndsWith(".sql")
+                ))
+            }");
+
+        // Find the first file name containing a digit
+        Console.WriteLine($"First file name containing a digit: " +
+            $"{files.Find(f => f.Any(n => char.IsDigit(n)))}");
+
+        Console.ReadKey();
+    }
+}
