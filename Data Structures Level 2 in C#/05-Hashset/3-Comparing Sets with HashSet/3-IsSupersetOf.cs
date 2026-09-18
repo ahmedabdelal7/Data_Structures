977a4ad7b085d932509b27b5b953f015b3d21872
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-09-18 19:59:48 +0300

    Comparing Sets

---
+++ Data Structures Level 2 in C#/05-Hashset/3-Comparing Sets with HashSet/3-IsSupersetOf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<int> set1 = new HashSet<int> { 1, 2, 3, 4, 5 };
+        HashSet<int> set2 = new HashSet<int> { 2, 3 };
+
+        // Is set1 including set2, or set1 is mother of set2
+        Console.WriteLine("set1 is a superset of set2: " + set1.IsSupersetOf(set2));
+        Console.ReadKey();
+    }
+}
