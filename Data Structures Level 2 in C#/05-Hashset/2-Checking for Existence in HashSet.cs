b7d516dd4517a49cc85f92a2c638c729ce8ef845
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-09-18 16:04:01 +0300

    HashSet

---
+++ Data Structures Level 2 in C#/05-Hashset/2-Checking for Existence in HashSet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Creating and populating a HashSet
+        HashSet<string> fruits = new HashSet<string> { "Apple", "Banana", "Cherry" };
+
+
+        // Checking if "Apple" is in the HashSet
+        if (fruits.Contains("Apple"))
+        {
+            Console.WriteLine("Apple is in the HashSet");
+        }
+        else
+        {
+            Console.WriteLine("Apple is not in the HashSet");
+        }
+    }
+}
