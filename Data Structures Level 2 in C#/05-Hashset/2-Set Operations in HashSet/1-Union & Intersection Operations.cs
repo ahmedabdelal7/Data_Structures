d269f145c2241c7f4b9f330e09c8def5c82b66e1
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-09-18 16:50:42 +0300

    Set Operations in HashSet

---
+++ Data Structures Level 2 in C#/05-Hashset/2-Set Operations in HashSet/1-Union & Intersection Operations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<int> set1 = new HashSet<int> { 1, 2, 3 };
+        HashSet<int> set2 = new HashSet<int> { 3, 4, 5 };
+
+
+        // Union of set1 and set2
+        set1.UnionWith(set2);
+
+        // Intersection of set1 and set2
+        //set1.IntersectWith(set2);
+
+
+        Console.WriteLine("Union of sets:");
+        foreach (int item in set1)
+        {
+            Console.WriteLine(item);
+        }
+        Console.ReadKey();
+    }
+}
---
+++ Data Structures Level 2 in C#/05-Hashset/2-Set Operations in HashSet/2-Difference Operation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<int> set1 = new HashSet<int> { 1, 2, 3 };
+        HashSet<int> set2 = new HashSet<int> { 3, 4, 5 };
+
+
+        // Removing elements of set2 from set1
+        // Elements that exist in set1 and do not exist in set2
+        set1.ExceptWith(set2);
+
+
+        Console.WriteLine("Difference of sets (set1 - set2):");
+        foreach (int item in set1)
+        {
+            Console.WriteLine(item);
+        }
+        Console.ReadKey();
+
+    }
+}
