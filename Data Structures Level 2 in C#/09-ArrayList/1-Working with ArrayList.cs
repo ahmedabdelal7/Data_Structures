18ab785261dbb67f0d1ace4c488062bcb1ac51cc
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-09-19 17:28:36 +0300

    ArrayList

---
+++ Data Structures Level 2 in C#/09-ArrayList/1-Working with ArrayList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList list = new ArrayList();
+        list.Add(10);
+        list.Add("Ahmed");
+        list.Add(false);
+
+
+        Console.WriteLine("Elements in ArrayList:");
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+
+        list.Remove(10); // Removing an element
+        Console.WriteLine("After removing an element:");
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+        Console.ReadKey();
+    }
+}
