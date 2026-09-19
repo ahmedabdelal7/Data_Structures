14822ed8d54fa76cec2384ddedd149e6a3ac274d
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-09-19 18:20:10 +0300

    Stacks

---
+++ Data Structures Level 2 in C#/11-Stacks/Stacks In C#.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<int> stack = new Stack<int>();
+
+
+        // Pushing elements onto the stack
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+
+        // Peeking at the top element
+        Console.WriteLine("Top element: " + stack.Peek());
+
+
+        // Popping elements from the stack
+        Console.WriteLine("Popped: " + stack.Pop());
+        Console.WriteLine("Popped: " + stack.Pop());
+
+
+        // Checking if the stack is empty
+        if (stack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty.");
+        }
+        else
+        {
+            Console.WriteLine("Top element: " + stack.Peek());
+        }
+
+        // Clearing the stack
+        stack.Clear();
+    }
+}
+
